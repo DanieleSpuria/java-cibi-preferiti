3dca16419719f03a883696550d21585c1813d024
DanieleSpuria <123416873+DanieleSpuria@users.noreply.github.com>
2023-09-13 17:21:39 +0200

    array cibi

---
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -0,0 +1,14 @@
+package org.lessons.java.array;
+
+public class CibiPreferiti {
+
+	public static void main(String[] args) {
+		
+		String[] cibi = { "spaghetti",  "pomodoro", "cipolla", "parmigiana", "pizza" };
+		
+		System.out.println("lunghezza array: " + cibi.length);
+		System.out.println("primo: " + cibi[0]);
+		System.out.println("ultimo: " + cibi[cibi.length - 1]);
+		System.out.println("mediano: " + cibi[cibi.length / 2]);
+	}
+}
