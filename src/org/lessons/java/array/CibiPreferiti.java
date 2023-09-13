58341c38ed52a42f7e7a187e3d1c3923a17d9de2
DanieleSpuria <123416873+DanieleSpuria@users.noreply.github.com>
2023-09-13 17:24:32 +0200

    add cibo

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -4,11 +4,11 @@ public class CibiPreferiti {
 
 	public static void main(String[] args) {
 		
-		String[] cibi = { "spaghetti",  "pomodoro", "cipolla", "parmigiana", "pizza" };
+		String[] cibi = { "spaghetti",  "pomodoro", "cipolla", "parmigiana", "tonno", "pizza" };
 		
 		System.out.println("lunghezza array: " + cibi.length);
 		System.out.println("primo: " + cibi[0]);
 		System.out.println("ultimo: " + cibi[cibi.length - 1]);
-		System.out.println("mediano: " + cibi[cibi.length / 2]);
+		System.out.println("mediano: " + cibi[(cibi.length / 2) - 1]);
 	}
 }
